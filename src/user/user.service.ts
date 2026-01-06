ba770edd011d7b660f2039ee6747bf8e9e1a74c7
Bhanu Sri <bhanuperuri4@gmail.com>
2026-01-06 16:42:53 +0530

    Fix user creation with password hashing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,46 +3,59 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+    private readonly userRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.create(createUserDto);
+  // CREATE USER ✅
+  async create(createUserDto: CreateUserDto) {
+    // hash password
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const user = this.userRepository.create({
+      email: createUserDto.email,
+      password: hashedPassword,
+    });
+
     return this.userRepository.save(user);
   }
 
-  findAll() {
+  // FIND ALL
+  async findAll() {
     return this.userRepository.find();
   }
 
+  // FIND ONE
   async findOne(id: number) {
-  const user = await this.userRepository.findOneBy({ id });
+    const user = await this.userRepository.findOneBy({ id });
 
-  if (!user) {
-    throw new NotFoundException(`User with id ${id} not found`);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return user;
   }
 
-  return user;
-}
+  // UPDATE
+  async update(id: number, updateUserDto: any) {
+    const user = await this.findOne(id);
 
- 
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
 
-
-  remove(id: number) {
-    return this.userRepository.delete(id);
-
+    Object.assign(user, updateUserDto);
+    return this.userRepository.save(user);
   }
 
-  update(id: number, updateUserDto: any) {
-  return {
-    message: `User ${id} updated successfully`,
-    data: updateUserDto,
-  };
-}
-
+  // DELETE
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.userRepository.remove(user);
+  }
 }
